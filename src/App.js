1a2267694d505fd13a989cc63218a00b1e949bb9
priyashanmugam2512 <priyashanmugam2512@gmail.com>
2024-03-29 13:45:47 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import './App.css';
+import Screen_page from './Components/Screen_page';
+import './External.css';
+import Segment_popup from './Components/Segment_popup';
+function App() {
+  return (
+   <>
+    <Screen_page/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/Segment_popup.js
@@ -0,0 +1,126 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+export default function Segment_popup({ onCancel }) {
+  const [segmentName, setSegmentName] = useState('');
+  const [selectedSchemas, setSelectedSchemas] = useState([]);
+  const [newSchema, setNewSchema] = useState('');
+  const schemaOptions = [
+    { label: 'First Name', value: 'first_name' },
+    { label: 'Last Name', value: 'last_name' },
+    { label: 'Gender', value: 'gender' },
+    { label: 'Age', value: 'age' },
+    { label: 'Account Name', value: 'account_name' },
+    { label: 'City', value: 'city' },
+    { label: 'State', value: 'state' }
+  ];
+  let link="https://webhook.site/e78d4f76-7264-4294-bd34-ec94a283a88d"
+  const handleAddSchema = () => {
+    if (newSchema && !selectedSchemas.includes(newSchema)) {
+      setSelectedSchemas([...selectedSchemas, newSchema]);
+      setNewSchema('');
+    }
+  };
+
+  const handleRemoveSchema = (index) => {
+    const updatedSchemas = [...selectedSchemas];
+    updatedSchemas.splice(index, 1);
+    setSelectedSchemas(updatedSchemas);
+  };
+  function postData(e){
+    e.preventDefault();
+    console.log("Segment Name:", segmentName);
+    console.log("Selected Schemas:", selectedSchemas);
+    const data = {
+      segment_name: segmentName,
+      schema: selectedSchemas.map(schema => ({ [schema.value]: schema.label }))
+    };
+    console.log(data)
+    axios.post(link,data)
+    alert("Your segment are save successfully");
+  }
+  return (
+    <>
+      <div className='popup_heading_section'>
+        <span className="material-symbols-outlined">chevron_left</span>
+        <h1>Saving Segment</h1>
+      </div>
+      <div className='form_section'>
+        <form onSubmit={postData}>
+          <div className='input_segment_section'>
+            <label htmlFor='name'>Enter the Name of the Segment</label>
+            <input type='text' id='name' placeholder="Name of the Segment" value={segmentName} onChange={(e) => setSegmentName(e.target.value)} />
+          </div>
+          <div className='drop_down_section'>
+            <p className='note_text'>To save  your segment, you need to add the schemas to build the query</p>
+            <div className='traits_section'>
+              <div className='user_traits'><span className='user_traits_dot'></span> <span className='user_text'>-User Traits</span></div>
+              <div className='group_traits'> <span className='group_traits_dot'></span> <span className='group_text'>-Group Traits</span></div>
+            </div>
+            <div>
+              <div className={selectedSchemas.length > 0 ? 'blueBorder' : ''}>
+                {selectedSchemas.map((schema, index) => (
+                  <div key={index} className='new_drop_down'>
+                    <span className={schema === 'account_name' || schema === 'state' || schema === 'city' ? 'group_traits_dot' : 'user_traits_dot'}></span>
+                    <select
+                      value={schema}
+                      onChange={(e) => {
+                        const updatedSchemas = [...selectedSchemas];
+                        updatedSchemas[index] = e.target.value;
+                        setSelectedSchemas(updatedSchemas);
+                      }}
+                    >
+                      <option value="" disabled>Select schema</option>
+                      {schemaOptions
+                        .filter(option => !selectedSchemas.includes(option.value) || option.value === schema)
+                        .map(option => (
+                          <option key={option.value} value={option.value} >{option.label}</option>
+                        ))}
+                    </select>
+                    <div className='remove_btn_section'>
+                      <span class="material-symbols-outlined"onClick={() => handleRemoveSchema(index)}>
+                        remove
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className='drop_down'>
+                <span className='gray_dot_traits'></span>
+                <select
+                  value={newSchema}
+                  onChange={(e) => setNewSchema(e.target.value)}
+                >
+                  <option value="" disabled>Add schema to segment</option>
+                  {schemaOptions
+                    .filter(option => !selectedSchemas.includes(option.value))
+                    .map(option => (
+                      <option key={option.value} value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+                <div className='remove_btn_section'>
+                      <span className="material-symbols-outlined"onClick={() => handleRemoveSchema}>
+                        remove
+                      </span>
+                    </div>
+              </div>
+              <div className='add_new_schema_link'>
+                <a onClick={handleAddSchema}><span className='plus_icon'>+</span><span className='text_add_new'>Add new schema</span></a>
+              </div>
+            </div>
+          </div>
+            <div className='save_btn_section'>
+                <button type="submit">Save the segment</button>
+            </div>
+          
+        </form>
+        <div className='cancel_btn_section'>
+            <button className='canel_btn' onClick={onCancel}>Cancel</button>
+            </div>
+
+      </div>
+    </>
+
+  )
+
+}
